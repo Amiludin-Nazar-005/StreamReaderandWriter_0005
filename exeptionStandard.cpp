70447e9768b6de5033e791c5274f3e13d6af4138
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-06-09 10:45:29 +0700

    membuatu catch untuk e.what

--- exeptionStandard.cpp
+++ exeptionStandard.cpp
@@ -15,4 +15,10 @@ int main()
         cout << data.at(5) << endl;
         //memanggil array elemen ke 5
     }
+    catch (exception& e){
+        //penangkap menggunakan obyek exception
+        cout <<e.what()<<endl;
+        //* akan di eksekusi karna array data hanya memiliki 3 elemen
+    }
+    
 }

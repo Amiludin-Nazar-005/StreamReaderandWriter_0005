2665b69754c64f6b7664e4b1de035aacfb80d36f
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-06-09 09:58:55 +0700

    membuat string dan oftream

---
+++ iofile.cpp
@@ -0,0 +1,13 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main (){
+    string baris;
+
+    ofstream outfile;
+
+    outfile.open ("contoh file.txt");
+    cout << ">=menulis file, \'q\' untuk keluar" << endl;
+}

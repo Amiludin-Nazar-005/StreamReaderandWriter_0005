62f1b33dd2c94b9f6a5340ca1d8b06112238ac4a
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-06-09 10:06:58 +0700

    menutuf file yang sudah di buat

--- iofile.cpp
+++ iofile.cpp
@@ -22,4 +22,10 @@ int main()
         // menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
+
+    // selesai dalam menulis sekarang tutup file nya
+
+    outfile.close();
+
+
 }

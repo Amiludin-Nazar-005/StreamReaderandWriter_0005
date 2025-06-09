ffd7adab0a2068b188591f869a510c3114a2469c
Amiludin-Nazar-005 <amiludin.nazar.ft24@mail.umy.ac.id>
2025-06-09 10:12:07 +0700

    operasi file dalam mode baca

--- iofile.cpp
+++ iofile.cpp
@@ -18,7 +18,8 @@ int main()
         // mendapatkan setiap karakter dalam satu garis
         getline(cin, baris);
         // loop akan berhenti jika anda masukkan karakter q
-        if (baris == "q")break;
+        if (baris == "q")
+            break;
         // menulis dan memasukkan nilai dari 'baris' ke dalam file
         outfile << baris << endl;
     }
@@ -27,5 +28,23 @@ int main()
 
     outfile.close();
 
+    // operasi file dalam mode membaca
+    ifstream infile;
+    // menunjuk ke sebuah file untuk membuka
+    infile.open("contohfile.txt");
 
+    cout << endl
+         << ">= membuka dan membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        //tutup file tersebut setelah selesai
+        infile.close();
+    }
 }
